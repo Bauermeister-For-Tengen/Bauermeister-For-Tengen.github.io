4d011381747b7778869012031440049ce863de34
markusbkk <happyferret2022@gmail.com>
2023-02-27 02:54:01 +0100

    Update HomepageFeatures component
    Import profile image

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 import Translate from '@docusaurus/Translate';
+import profile from '@site/static/img/profile.jpg';
 
 function Feature({ Svg, title, description, background }) {
   return (
