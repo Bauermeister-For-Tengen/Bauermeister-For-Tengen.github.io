e37c8b096b19b1df7ef832e7d64bc4a9f42769e4
markusbkk <happyferret2022@gmail.com>
2023-03-03 15:48:45 +0100

    Update Home component
    Add News button

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,18 +8,27 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/category/plattform">
-            Plattform
-          </Link>
+        <div className={clsx("row", styles.buttons)}>
+          <div className="col">
+            <Link
+              className="button button--secondary button--lg button--block col--2 col--offset-1 shadow--md"
+              to="/docs/category/plattform">
+              Plattform
+            </Link>
+          </div>
+          <div className="col">
+            <Link
+              className="button button--secondary button--lg button--block col--2 col--offset-1 shadow--md"
+              to="/blog">
+              Neuigkeiten
+            </Link>
+          </div>
         </div>
       </div>
     </header>
@@ -27,7 +36,7 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Willkommen auf der Webseite von ${siteConfig.title}`}
